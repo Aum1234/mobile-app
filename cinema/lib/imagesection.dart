2f60b2cab23af53c2678c776a6765a0e6ddf1277
Aum1234 <lalida.am.62@ubu.ac.th>
2021-12-02 20:17:02 +0700

    add

---
+++ cinema/lib/imagesection.dart
@@ -0,0 +1,21 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class ImageSection extends StatelessWidget {
+const ImageSection({Key? key}) : super(key: key);
+
+@override
+Widget build(BuildContext context) {
+return Container(
+//color: Colors.blue,
+padding:EdgeInsets.all(8),
+width: 1020,
+height: 200,
+child:Stack(children:[
+Image.network('https://assets.change.org/photos/7/ni/oo/qJnioOxfedlWlva-800x450-noPad.jpg?1529485013', width: 1020, height:400),
+CircleAvatar(backgroundImage: AssetImage('cinema1.jpg')),
+]));
+
+}
+}
